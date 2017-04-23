f2600f987741279e9aa3628d05cebf8506d09ca9
Joey Wang <wxluckly@gmail.com>
2017-04-23 23:50:47 +0800

    coding

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :ssh_options, {user: 'www'}
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/file')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/home/railsu/.rvm/gems/ruby-2.2.3/:$PATH" }
